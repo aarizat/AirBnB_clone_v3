2bbcc2ac89b405e9619198c1071202611a8ee05e
Sebastian_Orozco <omsebas@gmail.com>
2021-01-27 03:59:01 +0000

    Api version 01

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@ Display status code.
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
 @app_views.route("/status")
@@ -12,3 +19,18 @@ def display_status():
     Return status code OK
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def display_count():
+    """
+    Return the count of objects by type
+    """
+    return {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+        }
