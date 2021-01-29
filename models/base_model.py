528a9e38e24c4882f8c2e33eea8f49260a2c2ed6
Sebastian_Orozco <omsebas@gmail.com>
2021-01-29 19:33:02 +0000

    Change on the to_dict()

--- models/base_model.py
+++ models/base_model.py
@@ -67,7 +67,7 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if not disk:
+        if not disk and new_dict.__class__ == "User":
             new_dict.pop("password")
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
